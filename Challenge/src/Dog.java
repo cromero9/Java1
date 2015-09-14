bc47fbc385bb7a641b4f7bbeba2019c551727084
cromero9 <cromero9@mail.stmarytx.edu>
2015-09-14 13:33:25 -0500

    Challenge classes (abstract class)

---
+++ Challenge/src/Dog.java
@@ -0,0 +1,28 @@
+
+public class Dog extends Animal {
+	
+	Dog(String name, String breed, int age){
+	super(name, breed, age);
+	
+	
+	
+}
+
+	
+	public void makeNoise() {
+		
+		
+		System.out.print("Bark ");
+		
+	}
+
+	
+	public void uniqueSkill() {
+		
+		System.out.print("Runs ");
+		
+	}
+	
+	
+	
+}
